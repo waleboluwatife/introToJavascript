fc304a86b97df85d1d703b0cee882822c442502c
Your NameMike <waleboluwatife@gail.com>
2022-06-23 23:38:48 +0100

    Initial Commit

---
+++ prompt.js
@@ -0,0 +1,7 @@
+let person = {
+    name : "Michael",
+    height : 5.7,
+    country : "Nigeria",
+}
+
+alert('My name is '+ person.name +'.\nI am '+ person.height + 'ft in height, and I am from ' + person.country +'.');
